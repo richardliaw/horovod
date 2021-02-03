f362724b3ba78014d4065aac737493d45fe79d50
Richard Liaw <rliaw@berkeley.edu>
2021-02-03 12:36:08 -0800

    lradjust
    Signed-off-by: Richard Liaw <rliaw@berkeley.edu>

--- cifar10_ray_elastic.py
+++ cifar10_ray_elastic.py
@@ -14,10 +14,12 @@ import torch.nn.functional as F
 import torch.optim as optim
 # from torch.utils.tensorboard import SummaryWriter
 from torchvision import datasets, transforms
+from horovod.ray.elastic import TestDiscovery
 
 import horovod.torch as hvd
 from horovod.torch.elastic.sampler import ElasticSampler
 from horovod.ray.elastic import RayHostDiscovery
+from horovod.ray import ray_logger
 
 # Training settings
 parser = argparse.ArgumentParser(
@@ -29,6 +31,9 @@ parser.add_argument(
     '--checkpoint-format',
     default='./checkpoint-{epoch}.pth.tar',
     help='checkpoint file format')
+parser.add_argument(
+    '--use-ckpt', type=bool, default=False,
+    help="restore from checkpoint if possible")
 parser.add_argument(
     '--data-dir', default='./new_data', help='cifar10 dataset directory')
 
@@ -47,7 +52,7 @@ parser.add_argument(
     '--forceful', action="store_true",
     help="Removes the node upon deallocation (non-gracefully).")
 parser.add_argument(
-    '--change-frequency-s', type=int, default=40, help='random seed')
+    '--change-frequency-s', type=int, default=60, help='random seed')
 
 # Elastic Horovod settings
 parser.add_argument(
@@ -70,89 +75,13 @@ parser.add_argument(
 args = parser.parse_args()
 
 
-class TestDiscovery(RayHostDiscovery):
-    def __init__(self,
-                 min_hosts,
-                 max_hosts,
-                 change_frequency_s,
-                 use_gpu=False,
-                 cpus_per_slot=1,
-                 gpus_per_slot=1,
-                 graceful=True):
-        super().__init__(
-            use_gpu=use_gpu,
-            cpus_per_slot=cpus_per_slot,
-            gpus_per_slot=gpus_per_slot)
-        self._min_hosts = min_hosts
-        self._graceful = graceful
-        self._max_hosts = max_hosts
-        self._change_frequency_s = change_frequency_s
-        self._last_reset_t = None
-        self._removed_hosts = set()
-
-    def add_host(self, hosts):
-        available_hosts = self._removed_hosts & hosts.keys()
-        if available_hosts:
-            host = random.choice(list(available_hosts))
-            print('ADD HOST', host, hosts, self._removed_hosts)
-            self._removed_hosts.remove(host)
-
-        else:
-            print("No hosts to add.")
-
-    def remove_host(self, hosts):
-        good_hosts = [k for k in hosts if k not in self._removed_hosts]
-
-        from ray.autoscaler._private.commands import kill_node
-        if good_hosts:
-            if self._graceful:
-                host = random.choice(good_hosts)
-            else:
-                host = kill_node(
-                    os.path.expanduser("~/ray_bootstrap_config.yaml"), True,
-                    False, None)
-        print('REMOVE HOST', host, hosts, self._removed_hosts)
-        self._removed_hosts.add(host)
-
-    def change_hosts(self, hosts):
-        for host in self._removed_hosts:
-            if host not in hosts:
-                self._removed_hosts.remove(host)
-        current_hosts = len(hosts) - len(self._removed_hosts)
-        if current_hosts <= self._min_hosts:
-            self.add_host(hosts)
-        elif current_hosts >= self._max_hosts:
-            self.remove_host(hosts)
-        else:
-            if random.random() < 0.5:
-                self.add_host(hosts)
-            else:
-                self.remove_host(hosts)
-
-    def find_available_hosts_and_slots(self):
-        t = time.time()
-        if self._last_reset_t is None:
-            self._last_reset_t = t
-        hosts = super().find_available_hosts_and_slots()
-        if t - self._last_reset_t >= self._change_frequency_s:
-            self.change_hosts(hosts)
-            self._last_reset_t = t
-        print(f"Total hosts: {len(hosts)}")
-        remaining = {
-            k: v
-            for k, v in hosts.items() if k not in self._removed_hosts
-        }
-        print(f"Remaining hosts: {len(remaining)} -- {remaining}")
-        return remaining
-
-
 def load_data():
     # Horovod: limit # of CPU threads to be used per worker.
     torch.set_num_threads(4)
 
     # When supported, use 'forkserver' to spawn dataloader workers instead of 'fork' to prevent
     # issues with Infiniband implementations that are not fork-safe
-    kwargs = {'num_workers': 0, 'pin_memory': True} if args.cuda else {}
+    kwargs = {'num_workers': 2, 'pin_memory': True} if args.cuda else {}
     # if (kwargs.get('num_workers', 0) > 0 and hasattr(mp, '_supports_context') and
     #         mp._supports_context and 'forkserver' in mp.get_all_start_methods()):
     # kwargs['multiprocessing_context'] = 'spawn'
@@ -179,7 +108,7 @@ def load_data():
             transform=transform_train)
     train_sampler = ElasticSampler(train_dataset)
     train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=64, sampler=train_sampler, **kwargs)
+        train_dataset, batch_size=128, sampler=train_sampler, **kwargs)
 
     val_dataset = datasets.CIFAR10(
         root=args.data_dir,
@@ -188,12 +117,57 @@ def load_data():
         transform=transform_test)
     val_sampler = ElasticSampler(val_dataset)
     val_loader = torch.utils.data.DataLoader(
-        val_dataset, batch_size=64, sampler=val_sampler, **kwargs)
+        val_dataset, batch_size=128, sampler=val_sampler, **kwargs)
 
     return train_loader, val_loader, train_sampler, val_sampler
 
 
-def train(state, train_loader, log_writer, verbose):
+class tqdm_callback:
+    def __init__(self):
+        self._progress_bar = None
+        self._current_epoch = None
+        self._world_size = None
+        self._mode = None
+
+    def __call__(self, info):
+        tqdm_mode = info["tqdm_mode"]
+        assert tqdm_mode in {"val", "train"}
+        reset = False
+        if self._mode != tqdm_mode or \
+                self._current_epoch != info["epoch"] or \
+                self._world_size != info["world_size"]:
+            reset = True
+            self._mode = tqdm_mode
+            self._current_epoch = info["epoch"]
+            self._world_size = info["world_size"]
+
+        if reset:
+            if self._progress_bar is not None:
+                self._progress_bar.close()
+            epoch = self._current_epoch + 1
+            self._progress_bar = tqdm(
+                total=info["total"],
+                desc=f'[mode={tqdm_mode}] Epoch     #{epoch}')
+
+        scoped = {k: v for k, v in info.items() if k.startswith(tqdm_mode)}
+        self._progress_bar.set_postfix(scoped)
+        self._progress_bar.update(1)
+
+
+class TensorboardCallback:
+    def __init__(self, logdir):
+        from torch.utils.tensorboard import SummaryWriter
+        self.log_writer = SummaryWriter(logdir)
+
+    def __call__(self, info):
+        tqdm_mode = info["tqdm_mode"]
+        epoch = info["epoch"]
+        for k, v in info.items():
+            if k.startswith(tqdm_mode):
+                self.log_writer.add_scalar(k, v, epoch)
+
+
+def train(state, train_loader):
     epoch = state.epoch
     batch_offset = state.batch
 
@@ -202,72 +176,64 @@ def train(state, train_loader, log_writer, verbose):
     train_loss = Metric('train_loss')
     train_accuracy = Metric('train_accuracy')
 
-    with tqdm(
-            total=len(train_loader),
-            desc='Train Epoch     #{}'.format(epoch + 1),
-            disable=not verbose) as t:
+    for batch_idx, (data, target) in enumerate(train_loader):
+        # Elastic Horovod: update the current batch index this epoch
+        # and commit / check for host updates. Do not check hosts when
+        # we commit as it would be redundant.
+        state.batch = batch_offset + batch_idx
+        if args.batches_per_commit > 0 and \
+                state.batch % args.batches_per_commit == 0:
+            state.commit()
+        elif args.batches_per_host_check > 0 and \
+                state.batch % args.batches_per_host_check == 0:
+            state.check_host_updates()
 
-        for batch_idx, (data, target) in enumerate(train_loader):
-            # Elastic Horovod: update the current batch index this epoch
-            # and commit / check for host updates. Do not check hosts when
-            # we commit as it would be redundant.
-            state.batch = batch_offset + batch_idx
-            if args.batches_per_commit > 0 and \
-                    state.batch % args.batches_per_commit == 0:
-                state.commit()
-            elif args.batches_per_host_check > 0 and \
-                    state.batch % args.batches_per_host_check == 0:
-                state.check_host_updates()
+        if args.cuda:
+            data, target = data.cuda(), target.cuda()
+        state.optimizer.zero_grad()
 
-            if args.cuda:
-                data, target = data.cuda(), target.cuda()
-            state.optimizer.zero_grad()
+        output = state.model(data)
+        train_accuracy.update(accuracy(output, target))
 
-            output = state.model(data)
-            train_accuracy.update(accuracy(output, target))
-
-            loss = F.cross_entropy(output, target)
-            train_loss.update(loss)
-            loss.backward()
-            state.optimizer.step()
-
-            t.set_postfix({
-                'loss': train_loss.avg.item(),
-                'accuracy': 100. * train_accuracy.avg.item()
+        loss = F.cross_entropy(output, target)
+        train_loss.update(loss)
+        loss.backward()
+        state.optimizer.step()
+        # Only log from the 0th rank worker.
+        if hvd.rank() == 0:
+            ray_logger.log({
+                "tqdm_mode": "train",
+                "train/loss": train_loss.avg.item(),
+                "train/accuracy": 100. * train_accuracy.avg.item(),
+                "total": len(train_loader),
+                "epoch": epoch,
+                "world_size": hvd.size()
             })
-            t.update(1)
-
-    if log_writer:
-        log_writer.add_scalar('train/loss', train_loss.avg, epoch)
-        log_writer.add_scalar('train/accuracy', train_accuracy.avg, epoch)
 
 
-def validate(state, val_loader, log_writer, verbose):
+def validate(state, val_loader):
     state.model.eval()
     val_loss = Metric('val_loss')
     val_accuracy = Metric('val_accuracy')
 
-    with tqdm(
-            total=len(val_loader),
-            desc='Validate Epoch  #{}'.format(state.epoch + 1),
-            disable=not verbose) as t:
-        with torch.no_grad():
-            for data, target in val_loader:
-                if args.cuda:
-                    data, target = data.cuda(), target.cuda()
-                output = state.model(data)
+    with torch.no_grad():
+        for data, target in val_loader:
+            if args.cuda:
+                data, target = data.cuda(), target.cuda()
+            output = state.model(data)
 
-                val_loss.update(F.cross_entropy(output, target))
-                val_accuracy.update(accuracy(output, target))
-                t.set_postfix({
-                    'loss': val_loss.avg.item(),
-                    'accuracy': 100. * val_accuracy.avg.item()
+            val_loss.update(F.cross_entropy(output, target))
+            val_accuracy.update(accuracy(output, target))
+
+            if hvd.rank() == 0:
+                ray_logger.log({
+                    "tqdm_mode": "val",
+                    "val/loss": val_loss.avg.item(),
+                    "val/accuracy": 100. * val_accuracy.avg.item(),
+                    "total": len(val_loader),
+                    "epoch": state.epoch,
+                    "world_size": hvd.size()
                 })
-                t.update(1)
-
-    if log_writer:
-        log_writer.add_scalar('val/loss', val_loss.avg, state.epoch)
-        log_writer.add_scalar('val/accuracy', val_accuracy.avg, state.epoch)
 
 
 def accuracy(output, target):
@@ -437,10 +403,11 @@ def run():
 
     # If set > 0, will resume training from a given checkpoint.
     resume_from_epoch = 0
-    for try_epoch in range(args.epochs, 0, -1):
-        if os.path.exists(args.checkpoint_format.format(epoch=try_epoch)):
-            resume_from_epoch = try_epoch
-            break
+    if args.use_ckpt:
+        for try_epoch in range(args.epochs, 0, -1):
+            if os.path.exists(args.checkpoint_format.format(epoch=try_epoch)):
+                resume_from_epoch = try_epoch
+                break
 
     # Load cifar10 dataset
     train_loader, val_loader, train_sampler, val_sampler = load_data()
@@ -489,15 +456,10 @@ def run():
 
     @hvd.elastic.run
     def full_train(state, train_loader, val_loader):
-        # Horovod: print logs on the first worker.
-        verbose = 1 if hvd.rank() == 0 else 0
-
-        # Horovod: write TensorBoard logs on first worker.
-        # log_writer = SummaryWriter(args.log_dir) if hvd.rank() == 0 else None
         log_writer = None
         while state.epoch < args.epochs:
-            train(state, train_loader, log_writer, verbose)
-            validate(state, val_loader, log_writer, verbose)
+            train(state, train_loader)
+            validate(state, val_loader)
             state.scheduler.step()
             save_checkpoint(state)
             end_epoch(state)
@@ -517,8 +479,11 @@ if __name__ == '__main__':
         change_frequency_s=args.change_frequency_s,
         use_gpu=True,
         cpus_per_slot=1,
-        graceful=not args.forceful)
+        _graceful=not args.forceful,
+        verbose=False)
     executor = ElasticRayExecutor(
         settings, use_gpu=True, cpus_per_slot=1, override_discovery=False)
     executor.start()
-    executor.run(run)
+    executor.run(run,
+        callbacks=[tqdm_callback(),
+                   TensorboardCallback(args.log_dir)])
